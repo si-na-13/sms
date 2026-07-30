af51eaed19ff7ae167ded03760073ab8c69b85a5
Sina Mathewos <sinamathewos@gmail.com>
2026-07-30 11:09:38 -0700

    gggg

--- sina.js
+++ sina.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 function showmessage() {
  alert("student added successfully!");
 
 }
-=======
-function sina1(){
-alert("hello its programming");
-}
-fun
->>>>>>> 1ec26b42023d4f55aa4d5d5c3c16ca08ea0b7846
